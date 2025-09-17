7281a2f0e2a764890e6b9644088c63b3ab8a4efc
manikandan.suresh <manikandan.suresh@zohocorp.com>
2025-09-17 23:28:17 +0530

    ASL model zip file unzipped and  used

--- ProjectFiles/Flask/camera.py
+++ ProjectFiles/Flask/camera.py
@@ -6,13 +6,17 @@ import os
 
 class Predictor:
     def __init__(self):
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        model_path = os.path.join(base_dir, "asl_model.h5")
+		base_dir = os.path.dirname(os.path.abspath(__file__))
+        zip_path = os.path.join(base_dir, "asl_model.zip")   
+        model_path = os.path.join(base_dir, "asl_model.h5") 
+        # Extract the model if it doesn't exist
+        if not os.path.exists(model_path):
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            	zip_ref.extractall(base_dir)
         self.model = load_model(model_path)
         self.index = ['A','B','C','D','E','F','G','H','I']
 
     def predict_frame(self, frame):
-        # Extract ROI
         roi = frame[150:350, 50:250]
         roi_resized = cv2.resize(roi, (64, 64))
 
